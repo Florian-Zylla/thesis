c395d934a860aaf130e363596d0910b5181e6aa3
Florian-Zylla <florian.zylla@gmail.com>
2019-12-10 16:02:26 +0100

    ground truth class desgin

--- rotational_step_width.py
+++ rotational_step_width.py
@@ -23,9 +23,77 @@ def rect_detect(array,c_min,c_max,start,end):
     return False
 
 
+class GroundTruth:
+    #class to extract an image of the arena from the unity environment topview, later detect the agent
+    #and calculate its position
+    gt_pos = (0, 0)
+    edge_top_left = []
+    edge_bottom_right = []
+    agent = []
+    w_TL, h_TL = (0, 0)
+    w_BR, h_BR = (0, 0)
+    w_AGENT, h_AGENT = (0, 0)
+    TL = (0, 0)
+    BR = (0, 0)
+    agent_color_up = 225 / 2
+    agent_color_low = 175 / 2
+    relation = 40 / 512
+
+    def __init__(self):
+
+        self.edge_top_left = cv.imread("edge_top_left.png", 0)
+        self.edge_bottom_right = cv.imread("edge_bottom_right.png", 0)
+        self.agent = cv.imread("mask_agent.png", 0)
+        self.w_TL, self.h_TL = self.edge_top_left.shape[::-1]
+        self.w_BR, self.h_BR = self.edge_bottom_right.shape[::-1]
+        self.w_AGENT, self.h_AGENT = self.agent.shape[::-1]
 
 
 
+
+
+    def findArena(self):
+        with mss.mss() as sct:
+            img = cv.cvtColor(np.array(sct.grab(sct.monitors[0])), cv.COLOR_BGRA2BGR)
+            if i == 1:
+                screenshot = cv.cvtColor(np.array(img), cv.COLOR_BGR2GRAY)
+
+                res_TL = cv.matchTemplate(screenshot, self.edge_top_left, method=cv.TM_SQDIFF)
+                res_BR = cv.matchTemplate(screenshot, self.edge_bottom_right, method=cv.TM_SQDIFF)
+
+                min_val1, max_val1, min_loc1, max_loc1 = cv.minMaxLoc(res_TL)
+                min_val2, max_val2, min_loc2, max_loc2 = cv.minMaxLoc(res_BR)
+
+                self.TL = (min_loc1[0] + self.w_TL, min_loc1[1] + self.h_TL)
+                self.BR = min_loc2
+
+
+
+
+
+
+
+    def update(self):
+        with mss.mss() as sct:
+            img = cv.cvtColor(np.array(sct.grab(sct.monitors[0])), cv.COLOR_BGRA2BGR)
+            arena = img[self.TL[1] - 10:self.BR[1] + 12, self.TL[0] - 10:self.BR[0] + 12]
+
+          
+            hsv_arena = cv.cvtColor(arena, cv.COLOR_BGR2HSV)
+            h_arena = hsv_arena[:, :, 0]
+            mask = cv.inRange(h_arena, self.agent_color_low, self.agent_color_up)
+
+            res = cv.matchTemplate(mask, self.agent, method=cv.TM_SQDIFF)
+            min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+
+            top_left = min_loc
+
+            self.gt_pos = ((top_left[0] + self.w_AGENT / 2) * self.relation, (512 - (top_left[1] + self.h_AGENT / 2)) * self.relation)
+            if min_val > 2500000:
+                self.gt_pos = (-1, -1)
+
+    def get(self):
+        return self.gt_pos
 
 # initial settings of the environment
 env = UnityEnvironment(
@@ -43,16 +111,17 @@ env = UnityEnvironment(
 #arena_config_in = ArenaConfig('../thesis/test_configs/ground_truth_test.yaml')
 arena_config_in = ArenaConfig('../AnimalAI-Olympics/examples/configs/allObjectsRandom.yaml')
 env_info = env.reset(arenas_configurations=arena_config_in)
-edge_top_left = cv.imread("edge_top_left.png", 0)
-edge_bottom_right = cv.imread("edge_bottom_right.png", 0)
-agent =cv.imread("mask_agent.png",0)
-w1, h1 = edge_top_left.shape[::-1]
-w2, h2 = edge_bottom_right.shape[::-1]
-w, h = agent.shape[::-1]
-agent_color_up = 225/2
-agent_color_low = 175/2
-relation = 40/512
+#edge_top_left = cv.imread("edge_top_left.png", 0)
+#edge_bottom_right = cv.imread("edge_bottom_right.png", 0)
+#agent =cv.imread("mask_agent.png",0)
+#w1, h1 = edge_top_left.shape[::-1]
+#w2, h2 = edge_bottom_right.shape[::-1]
+#w, h = agent.shape[::-1]
+#agent_color_up = 225/2
+#agent_color_low = 175/2
+#relation = 40/512
 
+gt = GroundTruth()
 display = d.Display()
 # initalisation
 compass = c.Compass()
@@ -72,40 +141,10 @@ for i in range(361):
 
     display.update(observation, compass)
     if not i == 0:
-        with mss.mss() as sct:
-            img = cv.cvtColor(np.array(sct.grab(sct.monitors[0])), cv.COLOR_BGRA2BGR)
-            if i == 1:
-                screenshot = cv.cvtColor(np.array(img), cv.COLOR_BGR2GRAY)
-
-                res1= cv.matchTemplate(screenshot, edge_top_left, method=cv.TM_SQDIFF)
-                res2 = cv.matchTemplate(screenshot, edge_bottom_right, method=cv.TM_SQDIFF)
-
-                min_val1, max_val1, min_loc1, max_loc1 = cv.minMaxLoc(res1)
-                min_val2, max_val2, min_loc2, max_loc2 = cv.minMaxLoc(res2)
-                # min_val2, max_val3, min_loc3, max_loc3 = cv.minMaxLoc(res3)
-                top_left1 = min_loc1
-                top_left2 = min_loc2
-                # top_left2 = min_loc3
-                bottom_right1 = (top_left1[0] + w1, top_left1[1] + h1)
-                bottom_right2 = (top_left2[0] + w2, top_left2[1] + h2)
-                # bottom_right2 = (top_left3[0] + w3, top_left3[1] + h3)
-
-            arena = img[bottom_right1[1] - 10:top_left2[1] + 12, bottom_right1[0] - 10:top_left2[0] + 12]
-
-            #hsv_arena = matplotlib.colors.rgb_to_hsv((arena/255.0).astype(np.float))
-            hsv_arena = cv.cvtColor(arena, cv.COLOR_BGR2HSV)
-            h_arena = hsv_arena[:,:,0]
-            mask = cv.inRange(h_arena, agent_color_low ,agent_color_up)
-
-            res1 = cv.matchTemplate(mask, agent, method=cv.TM_SQDIFF)
-            min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res1)
-
-            top_left = min_loc
-
-            agent_position = ((top_left[0]+ w/2)*relation, (512-(top_left[1] + h/2)) * relation)
-            if min_val > 2500000 :
-                agent_position = (-1,-1)
-            print("x,y = ", agent_position)
+        if i ==1:
+            gt.findArena()
+        gt.update()
+        print(gt.get())
 
 
 
