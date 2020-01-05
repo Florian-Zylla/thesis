430ddcb46f9974a54dc19bd7fcc3e3cb2b3a0a42
Florian-Zylla <florian.zylla@gmail.com>
2020-01-05 22:43:53 +0100

    example for usage of the all the basic classes

--- carrot_planner.py
+++ carrot_planner.py
@@ -7,6 +7,7 @@ import pygame
 import numpy as np
 import math
 import time
+import cv2 as cv
 
 
 import compass.compass as c
@@ -14,7 +15,18 @@ import display.display as d
 import groundTruth.groundTruth as gt
 import odometry.odometry as o
 import log.log as l
+import heading.heading as h
+import trajectory.trajectory as t
+import pathExecuter.pathExecuter as pe
 
+
+def diff_angles(a,b):
+    diff_theta = abs(a - b)%360
+    if diff_theta > 180:
+        diff_theta = 360 -diff_theta
+    if (a-b >= 0 and a-b <= 180) or (a-b<=-180 and a-b>=-360):
+        diff_theta *= -1
+    return diff_theta
 
 
 # initial settings of the environment
@@ -38,64 +50,74 @@ env_info = env.reset(arenas_configurations=arena_config_in)
 
 display = d.Display()
 compass = c.Compass()
-
+head = h.Heading()
 
 
 gt = gt.GroundTruth()
 odo = o.Odometry()
 log = l.Log(config)
 
-goal = (25,25)
+p1 = t.Trajectory(23,17,2)
+p1.add(20,10,2)
+p1.add(2,2,2)
+p1.add(3,20,2)
+p1.add(5,37,2)
 
-l_turn = 1
-l_forward = 0
+carrot_planner = pe.pathExecuter(p1)
 
-env_info = env.step([0, 0])
+
+#head.set(270)
+
+action = [0,0]
+
+env_info = env.step([0, 0]) #for top view init
+
+
 gt.findArena()
 gt.update()
 odo.set_pose(gt.get())
+
+
+
 # the agent will take 20 time steps as an example
-for i in range(800):
+for i in range(250):
 
     gt.update()
     # velocity
     speed = env_info['Learner'].vector_observations
-    odo.update_pose(speed,compass)
-
+    odo.update_pose(speed, head)
 
     # camera input
     observation = env_info['Learner'].visual_observations[0]
-    display.update(observation, compass, gt)
-    log.update(i, speed, compass, odo, gt)
+
+    log.update(i, speed, head, odo, gt)
+
+    display.update(observation, head, gt)
+
+
+
+
+
+
+    frame = (observation[0,:,:,:]*255).astype(np.uint8)
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    low_threshold = 80
+    ratio = 3
+    kernel_size = (3,3)
+    img_blur = cv.blur(gray, (3,3))
+    detected_edges = cv.Canny(img_blur, low_threshold, low_threshold*ratio, kernel_size)
+    #edges_high_thresh = cv.Canny(gray, 100, 150)
+    #display.show(detected_edges)
+
 
 
     hsv_img = matplotlib.colors.rgb_to_hsv((observation[0, :, :, :]).astype(np.float))
 
-    #half speed
-    if i%2:
-        l_forward = 0
-    else:
-        l_forward = 1
+    action = carrot_planner.calc_action(i,odo,head)
 
-    #rotation
-    delta_x = goal[0] -odo.get_pose()[0]
-    delta_z = goal[1] - odo.get_pose()[1]
-    dist = math.sqrt(math.pow(delta_x,2)+ math.pow(delta_z,2))
-    goal_theta = math.atan(delta_z/delta_x)
+    env_info = env.step([action])
 
-    diff_theta = math.degrees(goal_theta) - compass.get()
-    print
-
-
-
-
-
-    env_info = env.step([l_forward, l_turn])
-    if l_turn == 2:
-        compass.step_left()
-    if l_turn == 1:
-        compass.step_right()
-
+    head.update(action[1])
 
 
 log.save()
